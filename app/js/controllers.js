1410cf6104afd684eee98c1f0111cf51fa2d8f0e
Hung BCP <hungbcp@asiantech.vn>
2016-03-03 14:33:31 +0700

    Create app controllers and mapped to app routing

---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var templateControllers = angular.module('templateControllers', [
+  'ngRoute'
+]);
+
+templateControllers.controller('HomeController', [function () {
+  // Controller method for Home
+}]);
+
+templateControllers.controller('AboutController', [function () {
+  // Controller method for About
+}]);
